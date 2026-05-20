572ef4b809a018840014b00eb1c32cf10276a626
2DimBer3 <berezan4@mail.ru>
2026-05-20 16:58:00 +0300

    fix EvenGame

--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -6,31 +6,27 @@ import static hexlet.code.Engine.ROUNDS_COUNT;
 import static hexlet.code.Engine.runGame;
 
 public class EvenGame {
-
     private static final RandomGenerator RANDOM = RandomGenerator.getDefault();
 
     public static void playEvenGame(String userName) {
         final String mainQuestion = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-        final String[][] questionsAndAnswers = generateRoundsData();
-
-        runGame(mainQuestion, questionsAndAnswers, userName);
-    }
-
-    private static String[][] generateRoundsData() {
-        final int MAX_NUMBER = 20;
-        String[][] questionsAndAnswers = new String[ROUNDS_COUNT][2];
+        final String[][] roundsData = new String[ROUNDS_COUNT][2];
 
         for (int i = 0; i < ROUNDS_COUNT; i++) {
-            int number = RANDOM.nextInt(MAX_NUMBER);
-
-            String question = String.valueOf(number);
-            String correctAnswer = isEvenNumber(number) ? "yes" : "no";
-
-            questionsAndAnswers[i][0] = question;
-            questionsAndAnswers[i][1] = correctAnswer;
+            roundsData[i] = generateRoundData();
         }
 
-        return questionsAndAnswers;
+        runGame(mainQuestion, roundsData, userName);
+    }
+
+    private static String[] generateRoundData() {
+        final int MAX_NUMBER = 20;
+        int number = RANDOM.nextInt(MAX_NUMBER + 1);
+
+        String question = String.valueOf(number);
+        String correctAnswer = isEvenNumber(number) ? "yes" : "no";
+
+        return new String[]{question, correctAnswer};
     }
 
     private static boolean isEvenNumber(int number) {
